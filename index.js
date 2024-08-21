73cc3022343a11fcb98cf5db76724899a92f9d5b
Monica Vega Arrieta <33848220+MonicaVega28@users.noreply.github.com>
2024-08-20 19:58:54 -0500

    configure cors, bodyParser and /ruta-prueba

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import connection from "./database/connection.js";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 // Mensaje de bienvenida para verificar que ejecute bien la API de Node
 console.log("API Node en ejecucion");
@@ -11,8 +13,28 @@ connection();
 const app = express();
 const puerto = process.env.PORT || 3900;
 
-//Otras configuraciones
+//Configurar cors para hacer las peticiones correctamente
+app.use(cors({
+   origin: '*',
+   methods: 'GET, HEAD, PUT, PATH,POST, DELETE'
+}));
 
+//Decodificar los datos desde los formularios para convertirlos en objectos JS
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Configurar rutas del aplicativo
+
+//Ruta de prueba
+app.get('/ruta-prueba', (req, res) => {
+   return res.status(200).json(
+      {
+         'id': 1,
+         'name': 'Monica Vega',
+         'username': 'monicapvega'
+      }
+   );
+});
 
 
 // Configurar el Servidor para escuchar las peticiones HTTP
