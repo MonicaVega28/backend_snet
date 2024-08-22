ef9c2e0a8a98ee2735eb1cbb739fb7d54952abcc
Monica Vega Arrieta <33848220+MonicaVega28@users.noreply.github.com>
2024-08-21 20:01:07 -0500

    configure test routes for user,publication,follow

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import connection from "./database/connection.js";
 import bodyParser from "body-parser";
 import cors from "cors";
 import UserRoutes from "./routes/user.js"
+import PublicationRoutes from "./routes/publication.js";
+import FollowRoutes from "./routes/follow.js";
 
 // Mensaje de bienvenida para verificar que ejecute bien la API de Node
 console.log("API Node en ejecucion");
@@ -26,7 +28,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Configurar rutas del aplicativo
 app.use('/api/user', UserRoutes);
-
+app.use('/api/publication', PublicationRoutes);
+app.use('/api/follow', FollowRoutes);
 
 //Ruta de prueba
 app.get('/ruta-prueba', (req, res) => {
