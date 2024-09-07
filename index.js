5bff3a87fad20086f18aba25f9b360f788690684
Monica Vega Arrieta <33848220+MonicaVega28@users.noreply.github.com>
2024-09-06 19:22:40 -0500

    Backend ready for Render deployment

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ import express from "express";
 import connection from "./database/connection.js";
 import bodyParser from "body-parser";
 import cors from "cors";
-import UserRoutes from "./routes/user.js"
+import UserRoutes from "./routes/user.js";
 import PublicationRoutes from "./routes/publication.js";
 import FollowRoutes from "./routes/follow.js";
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 // Mensaje de bienvenida para verificar que ejecute bien la API de Node
 console.log("API Node en ejecucion");
@@ -16,10 +19,13 @@ connection();
 const app = express();
 const puerto = process.env.PORT || 3900;
 
-//Configurar cors para hacer las peticiones correctamente
+// Configurar cors para hacer las peticiones correctamente
 app.use(cors({
-   origin: '*',
-   methods: 'GET, HEAD, PUT, PATH,POST, DELETE'
+   origin: '*', // Permitir solicitudes desde cualquier origen
+   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+   // Métodos permitidos
+   preflightContinue: false,
+   optionsSuccessStatus: 204
 }));
 
 //Decodificar los datos desde los formularios para convertirlos en objectos JS
@@ -31,9 +37,19 @@ app.use('/api/user', UserRoutes);
 app.use('/api/publication', PublicationRoutes);
 app.use('/api/follow', FollowRoutes);
 
-// Configurar el Servidor para escuchar las peticiones HTTP
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Configuración para servir archivos estáticos (imágenes de avatar)
+app.use('/uploads/avatars', express.static(path.join(__dirname, 'uploads', 'avatars')));
+
+// Configuración para servir archivos estáticos (imágenes de publicaciones)
+app.use('/uploads/publications', express.static(path.join(__dirname, 'uploads', 'publications')));
+
+// Configurar el servidor Node
 app.listen(puerto, () => {
-   console.log("Servidor de Node ejecutandose en el puerto", puerto);
+  console.log("Servidor de Node ejecutándose en el puerto", puerto);
 });
 
 export default app;
